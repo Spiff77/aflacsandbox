4ec8273c6b6ef40ad46f4e89606aa2a62fb48f7b
Thomas Lhomme <lhomme.thomas@gmail.com>
2021-07-27 10:03:46 +0200

    aflac-sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Client} from './model/Client';
+import {Supplier} from './model/Supplier';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testangular';
+  title = 'My super app';
+  count = 0;
+
+  selectValue = '';
+
+  color = undefined;
+
+  classList = ['bold red'];
+
+  c = new Client('Thomas', 'Lhomme', true, 36);
+  s = new Supplier(1, 'Aflac', '4444444', 100);
+
+  addOne(): void {
+    this.count++;
+  }
+
+  computeStyle(): string {
+    console.log('Hello');
+    if (this.s.ca < 1000){
+      return 'low';
+    }else if (this.s.ca >= 1000 && this.s.ca < 5000){
+      return 'medium';
+    }else{
+      return 'high';
+    }
+  }
+
+  toggle(): void {
+    this.c.active = !this.c.active;
+  }
+
+  divClick(event: any): void {
+    console.log(event.target.innerHTML);
+  }
 }
